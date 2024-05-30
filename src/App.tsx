e8fd8a397fec2a97a698757ed8b3ca8859f7d3ba
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-05-30 06:00:39 +0530

    s.env(stripe publish key), package.json( dependencies get form strip website), App.jsx(CheckotPage), checkout.tsx(initial code for shwoing payment interface)

--- src/App.tsx
+++ src/App.tsx
@@ -28,28 +28,16 @@ const Toss = lazy(() => import("./pages/admin/apps/toss"));
 const NewProduct = lazy(() => import("./pages/admin/management/newproduct"));
 const ProductManagement = lazy(() => import("./pages/admin/management/productmanagement"));
 const TransactionManagement = lazy(() => import("./pages/admin/management/transactionmanagement"));
-// notfund page
-const NotFound = lazy(() => import("./pages/not-found"));
-
-
-
-
+const NotFound = lazy(() => import("./pages/not-found")); // notfund page
+const Checkout = lazy(() => import("./pages/checkout"));  // for stripe
 
 // main files lazy loading
 const Home = lazy(()=>import('./pages/home'));
 const Search = lazy(()=>import('./pages/search'));
 const Cart = lazy(()=>import('./pages/cart'));
 
-
-
-
-
-
 // not logged in route here user login itself
 const Login = lazy(()=>import('./pages/login'));
-
-
-
 
 // user need login to acces this route
 const Shipping = lazy(()=>import('./pages/shipping'));
@@ -57,9 +45,8 @@ const Orders = lazy(()=>import('./pages/orders'));
 const OrderDetails = lazy(()=>import( './pages/order-details'));
 
 
-
 const App = () => {
-
+// this is for taking user and loading
   const {user , loading} = useSelector((state:{userReducer:UserReducerInitialState})=> state.userReducer)
 
    const dispatch = useDispatch();
@@ -74,9 +61,6 @@ useEffect(() => {
   });
 }, []);
 
-
-
-
   return loading? <Loader/> :(
     <Router>
       {/* Header */}
@@ -87,33 +71,22 @@ useEffect(() => {
         <Route path='/search' element={<Search/>}/>
         <Route path='/cart' element={<Cart/>}/>
 
-
-
-
         {/* not logged in route */}
         <Route path='/login' element={<ProtectedRoute isAuthenticated={user ? false:true}>
           <Login/>
         </ProtectedRoute>}/>
-
-
-
-
 
         {/* user need login to acces these route */}
           <Route element = {<ProtectedRoute isAuthenticated={user? true:false}/>} >
           <Route path='/shipping' element={<Shipping/>}/>
           <Route path='/orders' element={<Orders/>}/>
         <Route path='/order/:id' element={<OrderDetails/>}/>
+        <Route path='/pay' element={<Checkout/>}/>
           </Route>
 
-
         {/* Admin Routes */}
-      
             <Route
-              element={
-                <ProtectedRoute isAuthenticated={true} adminOnly={true} admin={user?.role==="admin" ? true : false} />
-              }
-            >
+              element={<ProtectedRoute isAuthenticated={true} adminOnly={true} admin={user?.role==="admin" ? true : false} />}>
               <Route path="/admin/dashboard" element={<Dashboard />} />
               <Route path="/admin/product" element={<Products />} />
               <Route path="/admin/customer" element={<Customers />} />
@@ -126,15 +99,13 @@ useEffect(() => {
               <Route path="/admin/app/coupon" element={<Coupon />} />
               <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
               <Route path="/admin/app/toss" element={<Toss />} />
-
               {/* Management */}
               <Route path="/admin/product/new" element={<NewProduct />} />
-
               <Route path="/admin/product/:id" element={<ProductManagement />} />
-
               <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
-            </Route>;
 
+            </Route>;
+               {/* Not found route */}
             <Route path='*' element={<NotFound/>}/>
       </Routes>
        </Suspense>
